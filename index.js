e7dfef321f9a8c4594f5d1cd520a57d1ab44d936
vatiza <rakibur.mac@gmail.com>
2024-04-17 20:11:54 +0600

    vercel config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const food = require("./data/food.json");
 const categories = require("./data/categories.json");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("server is running");
@@ -18,3 +20,4 @@ app.get("/categories", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+module.exports = app
