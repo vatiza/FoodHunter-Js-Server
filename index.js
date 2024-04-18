71ea10a748b79c2388a37fb550c840c5fbf9737f
vatiza <rakibur.mac@gmail.com>
2024-04-18 10:43:22 +0600

    modify data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const food = require("./data/food.json");
-const categories = require("./data/categories.json");
+const country = require("./data/country.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -13,8 +13,23 @@ app.get("/", (req, res) => {
 app.get("/food", (req, res) => {
   res.send(food);
 });
-app.get("/categories", (req, res) => {
-  res.send(categories);
+app.get("/country", (req, res) => {
+  res.send(country);
+});
+app.get("/food/:id",(req,res)=>{
+  const id=req.params.id;
+  const selectedFood=food.find(n=>n.id===id);
+  res.send(selectedFood);
+});
+app.get('/country/:id',(req,res)=>{
+  const id=req.params.id;
+  if(id==0){
+    res.send(food);
+  }else{
+    const category=food.filter(n=>n.category_id===id);
+  res.send(category);
+  }
+  
 });
 
 app.listen(port, () => {
